348c64f97b133a7f346d242bb4658df3b5a99719
Zuhaitz Méndez Fernández de Aránguiz <zuhaitz@debian>
2026-01-11 15:51:21 +0000

    Plugins time.

---
+++ plugins/zprep_plugin.h
@@ -0,0 +1,30 @@
+
+#ifndef ZPREP_PLUGIN_H
+#define ZPREP_PLUGIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+// The Host provides this API to the Plugin.
+typedef struct
+{
+    // Context Information (Where are we?).
+    const char *filename;
+    int current_line;
+    FILE *out;       // Inline output (expression context)
+    FILE *hoist_out; // Hoisted output (file scope / top level)
+} ZApi;
+
+// The Plugin Function Signature.
+// Returns void. All output is done via 'api'.
+typedef void (*ZPluginTranspileFn)(const char *input_body, const ZApi *api);
+
+typedef struct
+{
+    char name[32];
+    ZPluginTranspileFn fn;
+} ZPlugin;
+
+typedef ZPlugin *(*ZPluginInitFn)(void);
+
+#endif
